ffb8ae23ccc0b78ef191e26ba5dff0c4b06eccd3
ahmed_eid <kingmedo954@gmail.com>
2021-02-27 11:44:29 +0200

    Database Modeling (done)

--- network/models.py
+++ network/models.py
@@ -3,4 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', related_name='followers')
+
+
+class Post(models.Model):
+    content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    time_created = models.DateTimeField(auto_now_add=True)
+    user_likes = models.ManyToManyField(User, related_name='post_likes')
+
+    class Meta:
+        ordering = ['-time_created']
+
+    def __str__(self):
+        return f'{self.owner} has wrote this at {self.time_created}'
